5ade83cffcfca7ff6914c82046cdae212f4a0f6b
TryingToImprove <privat.oliver.lassen@gmail.com>
2017-05-10 11:49:21 +0200

    Add test to ensure travis work

---
+++ src/test/java/travis/TravisTest.java
@@ -0,0 +1,12 @@
+package travis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TravisTest {
+    @Test
+    public void testIsTravisTesting() {
+        assertTrue(true);
+    }
+}
