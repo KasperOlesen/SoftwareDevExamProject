6021e553ecd7cf721b00ab82b4569c3cde25245b
Sindt <Sindt>
2017-05-11 13:40:37 +0200

    Rest service added

---
+++ src/main/java/softDevExam/controller/GutenbergController.java
@@ -0,0 +1,42 @@
+package softDevExam.controller;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+
+import softDevExam.persistence.GutenbergMysql;
+import softDevExam.persistence.GutenbergNeo4J;
+
+public class GutenbergController {
+
+	private GutenbergService service;
+
+	public GutenbergController() {
+	}
+
+	public GutenbergController(GutenbergMysql service) {
+		this.service = service;
+	}
+
+	public GutenbergController(GutenbergNeo4J service) {
+		this.service = service;
+	}
+
+	public ResponseBuilder getBooksByCity(String city) {
+		try {
+			return Response.status(Status.OK).entity(service.getBooksByCity(city));
+		} catch (Exception e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage());
+		}
+	}
+
+	public ResponseBuilder getCitiesByBook(String book) {
+		try {
+			return Response.status(Status.OK).entity(service.getCitiesByBook(book));
+		} catch (Exception e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage());
+		}
+
+	}
+
+}
---
+++ src/test/java/softDevExam/GutenbergControllerTest.java
@@ -0,0 +1,57 @@
+package softDevExam;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import softDevExam.controller.GutenbergController;
+import softDevExam.persistence.GutenbergMysql;
+
+public class GutenbergControllerTest {
+
+	@Mock
+	private GutenbergMysql mysqlService;
+
+	@InjectMocks
+	private GutenbergController controller = new GutenbergController();
+
+	private String city = "Ajax";
+	private String book = "The Bible, King James Version, Complete";
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetBooksByCityReturnBook() {
+		when(mysqlService.getBooksByCity(anyString())).thenReturn(book);
+
+		Response result = controller.getBooksByCity(city).build();
+		assertThat(result.getEntity(), is(equalTo(book)));
+	}
+
+	@Test
+	public void testGetCitiesByBookReturnCities() {
+		when(mysqlService.getCitiesByBook(anyString())).thenReturn(city);
+
+		Response result = controller.getCitiesByBook(book).build();
+		assertThat(result.getEntity(), is(equalTo(city)));
+	}
+
+}
