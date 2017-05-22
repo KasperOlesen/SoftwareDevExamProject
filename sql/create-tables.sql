a716fffdac9486cf6c5338227640e5ef104bdc15
TryingToImprove <privat.oliver.lassen@gmail.com>
2017-05-22 09:22:30 +0200

    Make it faster

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,54 +1,60 @@
-CREATE TABLE `testprojekt`.`cities` (
+CREATE TABLE `testprojekt3`.`cities` (
   `id` INT NOT NULL COMMENT '',
   `name` VARCHAR(255) NULL COMMENT '',
   `latitude` DECIMAL NULL COMMENT '',
   `longitude` DECIMAL NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '');
 
-CREATE TABLE `testprojekt`.`books` (
+CREATE TABLE `testprojekt3`.`books` (
   `id` VARCHAR(36) NOT NULL COMMENT '',
   `name` VARCHAR(255) NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '');
 
-CREATE TABLE `testprojekt`.`authors` (
+CREATE TABLE `testprojekt3`.`authors` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `name` VARCHAR(255) NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '');
 
 
-CREATE TABLE `testprojekt`.`book_author` (
+CREATE TABLE `testprojekt3`.`book_author` (
   `bookId` VARCHAR(36) NOT NULL COMMENT '',
   `authorId` INT NOT NULL COMMENT '',
   PRIMARY KEY (`bookId`, `authorId`)  COMMENT '',
   INDEX `author_idx` (`authorId` ASC)  COMMENT '',
   CONSTRAINT `author`
     FOREIGN KEY (`authorId`)
-    REFERENCES `testprojekt`.`authors` (`id`)
+    REFERENCES `testprojekt3`.`authors` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `book`
     FOREIGN KEY (`bookId`)
-    REFERENCES `testprojekt`.`books` (`id`)
+    REFERENCES `testprojekt3`.`books` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-CREATE TABLE `testprojekt`.`book_city´` (
+CREATE TABLE `testprojekt3`.`book_city` (
   `bookId` VARCHAR(36) NOT NULL COMMENT '',
   `cityId` INT NOT NULL COMMENT '',
   PRIMARY KEY (`bookId`, `cityId`)  COMMENT '',
   INDEX `city_idx` (`cityId` ASC)  COMMENT '',
   CONSTRAINT `book_c`
     FOREIGN KEY (`bookId`)
-    REFERENCES `testprojekt`.`books` (`id`)
+    REFERENCES `testprojekt3`.`books` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `city`
     FOREIGN KEY (`cityId`)
-    REFERENCES `testprojekt`.`cities` (`id`)
+    REFERENCES `testprojekt3`.`cities` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-ALTER TABLE `testprojekt`.`cities` 
+ALTER TABLE `testprojekt3`.`cities` 
 DROP COLUMN `longitude`,
 DROP COLUMN `latitude`,
 ADD COLUMN `location` POINT NULL COMMENT '' AFTER `name`;
+
+ALTER TABLE `testprojekt3`.`authors` 
+ADD INDEX `nameIndex` (`name` ASC)  COMMENT '';
+
+ALTER TABLE `testprojekt3`.`cities` 
+ADD INDEX `nameIndex` (`name` ASC)  COMMENT '';
